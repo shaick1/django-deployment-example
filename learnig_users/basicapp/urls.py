e21089ccb3b8cda2c0ef3649733669f1bd26adf2
shaickmohamed Sirajudeen <shaick.pop@gmail.com>
2020-08-02 20:08:07 +0530

    all files2

---
+++ learnig_users/basicapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from basicapp import views
+
+
+app_name = 'basicapp'
+
+urlpatterns = [
+    path('',views.index,name="index"),
+    path('register/',views.register,name="register"),
+    path('user_login/',views.user_login,name="user_login"),
+]
